272f6e317718eda5c09a0f0e5ea014f29ec4c458
Yunsik-Choi <choiys0311@gmail.com>
2023-06-16 02:26:12 +0900

    feat : BoardController 조회 인터페이스 정의

--- test/src/main/java/com/example/test/presentation/BoardController.java
+++ test/src/main/java/com/example/test/presentation/BoardController.java
@@ -5,6 +5,8 @@ import com.example.test.application.dto.BoardCreateRequest;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,5 +20,10 @@ public class BoardController {
     public ResponseEntity createBoard(@RequestBody BoardCreateRequest boardCreateRequest) {
         long id = boardService.create(boardCreateRequest);
         return ResponseEntity.created(URI.create("/board/" + id)).build();
+    }
+
+    @GetMapping("/board/{id}")
+    public ResponseEntity findBoardById(@PathVariable Long id) {
+        return ResponseEntity.ok().build();
     }
 }
