bab7d5723bf4b4eeb06a379fec7921ab1637daad
Yunsik-Choi <choiys0311@gmail.com>
2023-06-16 02:14:26 +0900

    test : 게시글 조회 인수 테스트 작성

--- test/src/test/java/com/example/test/acceptance/BoardAcceptanceSteps.java
+++ test/src/test/java/com/example/test/acceptance/BoardAcceptanceSteps.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import java.util.Map;
@@ -41,5 +42,29 @@ public class BoardAcceptanceSteps {
                 .post("/board")
                 .then().log().all()
                 .extract();
+    }
+
+    protected static ExtractableResponse<Response> 게시글_ID로_조회_요청(final long id) {
+        return RestAssured.given().log().all()
+                .when()
+                .get("board/{id}", id)
+                .then().log().all()
+                .extract();
+    }
+
+    protected static void 게시글_ID로_조회_요청_검증(
+            final long id,
+            final String title,
+            final String content,
+            final ExtractableResponse<Response> response
+    ) {
+        JsonPath jsonPath = response.jsonPath();
+
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
+                () -> assertThat(jsonPath.getLong("id")).isEqualTo(id),
+                () -> assertThat(jsonPath.getString("title")).isEqualTo(title),
+                () -> assertThat(jsonPath.getString("content")).isEqualTo(content)
+        );
     }
 }
--- test/src/test/java/com/example/test/acceptance/BoardAcceptanceTest.java
+++ test/src/test/java/com/example/test/acceptance/BoardAcceptanceTest.java
@@ -1,5 +1,7 @@
 package com.example.test.acceptance;
 
+import static com.example.test.acceptance.BoardAcceptanceSteps.게시글_ID로_조회_요청;
+import static com.example.test.acceptance.BoardAcceptanceSteps.게시글_ID로_조회_요청_검증;
 import static com.example.test.acceptance.BoardAcceptanceSteps.게시물_생성_요청;
 import static com.example.test.acceptance.BoardAcceptanceSteps.게시물_생성_요청_검증;
 
@@ -28,5 +30,25 @@ public class BoardAcceptanceTest extends AcceptanceTest {
 
         // Then
         게시물_생성_요청_검증(title, content, response);
+    }
+
+    /**
+     * Given : 게시글을 작성하고
+     * When : 게시글을 조회하면
+     * Then : 게시글이 조회된다.
+     */
+    @Test
+    void 게시글_조회() {
+        // Given
+        String title = UUID.randomUUID().toString();
+        String content = UUID.randomUUID().toString();
+        게시물_생성_요청(title, content);
+        long id = 1L;
+
+        // When
+        var response = 게시글_ID로_조회_요청(id);
+
+        // Then
+        게시글_ID로_조회_요청_검증(id, title, content, response);
     }
 }
